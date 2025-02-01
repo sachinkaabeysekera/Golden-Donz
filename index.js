3283fd86c90c2341ad883cc5355ae718377ec6fa
sachinkaabeysekera <102205045+sachinkaabeysekera@users.noreply.github.com>
2025-02-01 09:08:15 +0530

    index.js

--- index.js
+++ index.js
@@ -81,42 +81,33 @@ function createVideoElement(source) {
     return video;
 }
 
-function showSuccessPopup() {
-    alert("You have been Registered Successfully! Please go Back and Click the Already Registered button.");
-}
-  
+const form = document.forms['contact-form'];
+const responseMessage = document.getElementById('response-message');
 const scriptURL = 'https://script.google.com/macros/s/AKfycbyqeUdYFFiLiBPcF9hxiFR4ZknesEpT9IGOKlpy2kGAnq281FQd-Cev4FDiUPkZx3_TpA/exec';
-    const form = document.forms['contact-form'];
-    const responseMessage = document.getElementById('response-message');
 
-    form.addEventListener('submit', e => {
-        e.preventDefault();
+form.addEventListener('submit', e => {
+    e.preventDefault();
 
-        const timestamp = new Date().toLocaleString();
-
-    // Set the value of the hidden timestamp input field
+    const timestamp = new Date().toLocaleString();
     const timestampField = document.getElementById('timestamp');
     timestampField.value = timestamp;
 
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => response.text())
+        .then(result => {
+            if (result === 'registered_successfully') {
+                displayMessage("Registered Successfully!", "green");
+                showSuccessPopup();
+            } else if (result === 'already_registered') {
+                displayMessage("Already Registered!", "orange");
+            } else {
+                displayMessage("Try Again!", "red");
+            }
+        })})
         
 
-   fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-            .then(response => {
-                if (response.ok) {
-                    responseMessage.innerHTML = "Registered Successfully!";
-                    responseMessage.style.color = "green";
+function showSuccessPopup() {
+    alert("You have been registered successfully! Please go back and click the Already Registered button.");
+}
 
-                showSuccessPopup()
-
-
-                } else {
-                    responseMessage.innerHTML = error.message ;
-                    responseMessage.style.color = "red";
-                }
-            })
-            .catch(error => {
-                responseMessage.innerHTML = error.message ;
-                responseMessage.style.color = "red";
-            });
-    });
 
