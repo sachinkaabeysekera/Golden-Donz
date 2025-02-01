da48dbebb2e8715a708d0276582aaac423c573df
sachinkaabeysekera <102205045+sachinkaabeysekera@users.noreply.github.com>
2025-02-01 08:41:48 +0530

    index.js

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ function showSuccessPopup() {
     alert("You have been Registered Successfully! Please go Back and Click the Already Registered button.");
 }
   
-const scriptURL = "https://script.google.com/macros/s/AKfycbzd0gh1SrxfH41JQSAc3bLBGf2BcQdub_q8W051fpcOe9VkGz4u2aHdD8_oG9CRC7cHqg/exec";
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzd0gh1SrxfH41JQSAc3bLBGf2BcQdub_q8W051fpcOe9VkGz4u2aHdD8_oG9CRC7cHqg/exec';
     const form = document.forms['contact-form'];
     const responseMessage = document.getElementById('response-message');
 
